f8158d36d8aaa4368cbbce8e5672c15d845fca08
jiangleiyu <jianglei.yu@foxmail.com>
2020-03-18 18:07:25 +0800

    init vue demo

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
