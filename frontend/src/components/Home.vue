19625725029d8a3d56e67467a4070a126bcfe04c
jiangleiyu <jianglei.yu@foxmail.com>
2020-04-20 01:10:56 +0800

    项目布局

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <h1>hello</h1>
-  </div>
+<el-container class="home-container">
+  <!-- 头部 -->
+  <el-header>
+    <div>
+      <img src="../assets/logo.png" alt="" style="width: 50px; height: 50px">
+      <span>代码构建发布系统</span>
+    </div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </el-header>
+  <el-container>
+    <!-- 左侧 -->
+    <el-aside width="200px">Aside</el-aside>
+    <!-- 主体 -->
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+
 </template>
 
 <script>
@@ -15,5 +29,33 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang='less' scope>
+.home-container{
+  height: 100%;
+}
+
+.el-header{
+  background-color: #373D41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  >div{
+    display: flex;
+    align-items: center;
+    span{
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside{
+  background-color: #373D41;
+}
+
+.el-main{
+  background-color: #EAEDF1;
+}
 </style>
