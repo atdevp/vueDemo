74a7caea96ee133c2050c0086bd2917f70b6980d
jiangleiyu <jianglei.yu@foxmail.com>
2020-05-03 11:10:01 +0800

    base framework is ok

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,13 @@ import Home from '@/components/Home'
 import Login from '@/components/Login'
 import Welcome from '@/components/Welcome'
 import UserList from '@/components/users/UserList'
+import ProjectList from '@/components/projects/ProjectList'
+import ProjectAdd from '@/components/projects/ProjectAdd'
+import AppList from '@/components/applications/AppList'
+import AppAdd from '@/components/applications/AppAdd'
+import Compile from '@/components/cicd/Compile'
+import Deploy from '@/components/cicd/Deploy'
+import Rollback from '@/components/cicd/Rollback'
 
 Vue.use(Router)
 
@@ -15,7 +22,14 @@ export default new Router({
       redirect: '/welcome',
       children: [
         { path: '/welcome', component: Welcome },
-        { path: '/users/list', component: UserList }
+        { path: '/users/list', component: UserList },
+        { path: '/projects/list', component: ProjectList },
+        { path: '/projects/add', component: ProjectAdd },
+        { path: '/apps/list', component: AppList },
+        { path: '/apps/add', component: AppAdd },
+        { path: '/cicd/compile', component: Compile },
+        { path: '/cicd/deploy', component: Deploy },
+        { path: '/cicd/rollback', component: Rollback }
       ]
 
     },
